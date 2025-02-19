5ebcb72a92af9ff236817f52ca428d0c2500a588
Angie Hong <lsuvhyc1@gmail.com>
2025-02-19 13:50:09 +0800

    change origin

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Replace with your frontend's URL
+    origin: "http://localhost:3000", // Replace with your frontend's URL
     methods: "GET,POST,PUT,DELETE",
     credentials: true, // Allow cookies if needed
   }),
