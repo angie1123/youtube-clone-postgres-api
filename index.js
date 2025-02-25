c9c5eb5bd5e3284ec229ba53f7cdcb880879b0f5
Angie Hong <lsuvhyc1@gmail.com>
2025-02-25 15:11:17 +0800

    change variable datatype

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ let serviceAccount;
 try {
   // This will replace \\n with actual newlines, making it work properly when passing to Firebase Admin SDK.
   //g ensures all occurrences of \\n are replaced, not just the first one. 
-  serviceAccount = FIREBASE_SERVICE_ACCOUNT
+  serviceAccount = JSON.parse(FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, '\n'));
   // console.log(serviceAccount)
   console.log(typeof FIREBASE_SERVICE_ACCOUNT)
   
